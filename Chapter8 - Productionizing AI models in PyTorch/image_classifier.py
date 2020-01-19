a272766e5c3cd94459b306ff44041ca48202a2fb
jibinmathew69 <jibinmathew.69@gmail.com>
2020-01-19 14:08:56 +0530

    Add create_model

--- Chapter8 - Productionizing AI models in PyTorch/image_classifier.py
+++ Chapter8 - Productionizing AI models in PyTorch/image_classifier.py
@@ -5,3 +5,7 @@ from torchvision import models
 from PIL import Image
 import torchvision.transforms as transforms
 
+def create_model():
+
+    model_path = "image_model.pth"
+    model = models.densenet161(pretrained=True)
