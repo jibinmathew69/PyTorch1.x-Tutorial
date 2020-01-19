83e9be11ef115c73aa6f8e04e0714cf2570bd4cc
jibinmathew69 <jibinmathew.69@gmail.com>
2020-01-19 14:11:41 +0530

    Complete create_model

--- Chapter8 - Productionizing AI models in PyTorch/image_classifier.py
+++ Chapter8 - Productionizing AI models in PyTorch/image_classifier.py
@@ -7,5 +7,9 @@ import torchvision.transforms as transforms
 
 def create_model():
 
-    model_path = "image_model.pth"
+    model_path = "checkpoint_final.pth"
     model = models.densenet161(pretrained=True)
+    model.classifier = nn.Linear(2208, 102)
+    model.load_state_dict(torch.load(model_path, map_location='cpu'), strict=False)
+    model.eval()
+    return model
