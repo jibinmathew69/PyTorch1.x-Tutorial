390e73fbe6243532f2cbed9dd0296c527dde6309
jibinmathew69 <jibinmathew.69@gmail.com>
2020-01-19 17:53:58 +0530

    Completion prediction url

--- Chapter8 - Productionizing AI models in PyTorch/imageapp.py
+++ Chapter8 - Productionizing AI models in PyTorch/imageapp.py
@@ -1,5 +1,18 @@
-from flask import Flask, request
-from image_classifier import create_model, image_transformer, predict_flower
+from flask import Flask, request, jsonify
+from image_classifier import create_model, predict_flower
 
 app = Flask(__name__)
+model = create_model()
 
+@app.route('/predict', methods=['POST'])
+def predicted():
+    if 'image' not in request.files:
+        return jsonify({'error': 'Image not found'}), 400
+
+    image = request.files['image'].read()
+    flower_name = predict_flower(model, image)
+
+    return jsonify({'flower_name' : flower_name})
+
+if __name__ == '__main__':
+	app.run(debug=True)
