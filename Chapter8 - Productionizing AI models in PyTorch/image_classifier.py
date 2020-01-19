990baddd036029cb459302eb725d89f545350e2e
jibinmathew69 <jibinmathew.69@gmail.com>
2020-01-19 14:24:48 +0530

    Add transforms

--- Chapter8 - Productionizing AI models in PyTorch/image_classifier.py
+++ Chapter8 - Productionizing AI models in PyTorch/image_classifier.py
@@ -13,3 +13,12 @@ def create_model():
     model.load_state_dict(torch.load(model_path, map_location='cpu'), strict=False)
     model.eval()
     return model
+
+def image_transformer():
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])])
+    ])
